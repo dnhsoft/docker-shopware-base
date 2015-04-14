2b829f1598e5283ea02c5b7316639f85844faf88
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2015-04-14 17:34:21 +0300

    Unzupping the shop prepared for dev version

--- 4.x/unzip-shopware.sh
+++ 4.x/unzip-shopware.sh
@@ -2,31 +2,32 @@
 
 cd /shopware
 
-# unzip -n makes the unzip to skip overrwiting of existing files;
-# then set the rights and preserve the directories to be mounted;
+if [ -f ./demo.zip ]; then
 
-unzip -n -q shop.zip
-rm shop.zip
+    unzip -n -q shop.zip
+    rm shop.zip
 
-if [ -d recovery ]; then
-    mv recovery /swrecovery
+    if [ -d ./recovery ]; then
+        mv ./recovery /swrecovery
+    fi
+
+    if [ -d ./install ]; then
+        mv ./install /swinstall
+    fi
+
+    if [ -f ./demo.zip ]; then
+        unzip ./demo.zip
+        rm ./demo.zip
+    fi
+
+    mkdir -p /shopware/logs
+
+    chown -R www-data:www-data /shopware/
+
+    chmod 775 -R cache files logs media engine/Shopware/Plugins/Community
+
+    mv logs /swvolumes/logs
+    mv files /swvolumes/files
+    mv media /swvolumes/media
+
 fi
-
-if [ -d install ]; then
-    mv install /swinstall
-fi
-
-if [ -f demo.zip ]; then
-    unzip demo.zip
-    rm demo.zip
-fi
-
-mkdir -p /shopware/logs
-
-chown -R www-data:www-data /shopware/
-
-chmod 775 -R cache files logs media engine/Shopware/Plugins/Community
-
-mv logs /swvolumes/logs
-mv files /swvolumes/files
-mv media /swvolumes/media
