842074bac02e001b004eac294b928797a3e09544
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2016-03-15 07:37:46 +0200

    small bug fixed

--- 5.1.x/swtools/init-shop.sh
+++ 5.1.x/swtools/init-shop.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 cd /shopware/bin
+
+chmod +x console
+
 curl -s https://getcomposer.org/installer | php
 php console sw:generate:attributes
 php console sw:firstrunwizard:disable
 php console sw:theme:initialize
 
-chown www-data:www-data -R /shopware/var/cache/
+chown www-data:www-data -R /shopware
