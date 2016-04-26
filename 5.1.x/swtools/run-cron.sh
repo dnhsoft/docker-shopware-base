65d12a5b3921eae8f66e1b87bda7fc1ec14e597c
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2016-04-26 13:51:47 +0300

    Cronjobs removed and replaced with local bash script

--- 5.1.x/swtools/run-cron.sh
+++ 5.1.x/swtools/run-cron.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "sw:cron:run..."
+echo "["$(date "+%Y-%m-%d %H:%M:%S %Z")"]" "sw:cron:run..."
+
 php /shopware/bin/console sw:cron:run
 
 # restore the directories ownershop back to www-data
