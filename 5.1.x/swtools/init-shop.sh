3b0e1ae31d9012d24b20135d9c40e4f9c4cb0756
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2016-05-25 14:49:46 +0300

    composer install moved to the base docker image

--- 5.1.x/swtools/init-shop.sh
+++ 5.1.x/swtools/init-shop.sh
@@ -4,7 +4,6 @@ cd /shopware/bin
 
 chmod 744 console
 
-curl -s https://getcomposer.org/installer | php
 php console sw:generate:attributes
 php console sw:firstrunwizard:disable
 php console sw:theme:initialize
